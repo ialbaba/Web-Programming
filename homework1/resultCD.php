8a20f95f1f18ce9346aca1df38d9403ca951f501
Ibrahims Macbook <ibjbaba@gmail.com>
2021-02-23 13:02:55 -0600

    final changes to homework C

--- homework1/resultCD.php
+++ homework1/resultCD.php
@@ -96,7 +96,7 @@ $timeStamp = $time->format('Y-m-d H:i');
 $blob = array("FirstName"=>$fname, "LastName"=>$lname, "Q1"=>$trueFalse, "Q2"=>$statements, "Q3"=>$majors, "Q4"=>$math1, "Q5"=>$math2, "Timestamp"=>$timeStamp);
 echo(serialize($blob));
 $filePath = "results.txt";
-file_put_contents($filePath, $blob, FILE_APPEND);
+file_put_contents($filePath, serialize($blob), FILE_APPEND);
 ?>
 
 
