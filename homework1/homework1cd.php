e42db0ef73bc761d0597e6387421bf2085ea9600
Ibrahims Macbook <ibjbaba@gmail.com>
2021-02-23 12:45:01 -0600

    homework 1c

---
+++ homework1/homework1cd.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Homework 1</title>
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+</head>
+<body>
+    <h1>HOMEWORK 1</h1>
+    <h2>Quiz</h2>
+
+    <form action="resultCD.php" method="GET">
+        
+        <label for="firstName">First name:</label>
+        <input type="text" id="firstName" name="firstName"><br>
+        <label for="lastName">Last name:</label>
+        <input type="text" id="lastName" name="lastName"><br>
+        
+        <h3>Question 1:</h3>
+        <label for="Q1">Australia is wider than the moon:</label> <br>
+            <input type="radio" name="trueFalse" value="True">
+            <label for="isTrue">True</label><br>
+            <input type="radio" name="trueFalse" value="False">
+            <label for="isFalse">False</label><br>
+
+        <h3>Question 2:</h3>
+        <label for="statements">Select all true statements</label> <br>
+        <input type="checkbox" id="statement1" name="statements" value="scrabble">
+        <label for="statements">K is worth four points in Scrabble</label><br>
+        <input type="checkbox" id="statement2" name="statements" value="madonna">
+        <label for="statements">Madonna's real name is Madonna</label><br>
+        <input type="checkbox" id="statement3" name="statements" value="periodicTable">
+        <label for="statements">The only letter not in the periodic table is the letter I</label><br>
+
+        <h3>Question 3:</h3>
+        <label for="majors">What major is not offered in the SCI School:</label>
+        <select name="majors" id="majors">
+            <option value="infsci">Information Science</option>
+            <option value="compsci">Computer Science</option>
+            <option value="compbio">Biology</option>
+            <option value="other">Other</option>
+        </select>
+        <h3 class = "mathSection">Math Section!</h3>
+        <h3>Question 4:</h3>
+        <label for="mathQuestion1">2^6?</label>
+        <input type="text" id="mathQuestion1" name="mathQuestion1"><br>
+
+        <h3>Question 5:</h3>
+        <label for="mathQuestion2">Log(1)?</label>
+        <input type="text" id="mathQuestion2" name="mathQuestion2"><br>
+
+        <br>
+        <br>
+
+        <input type="submit" value="Submit">
+
+    </form>
+
+</body>
+</html>
---
+++ homework1/resultCD.php
@@ -0,0 +1,101 @@
+
+<?php
+
+$fname = $_GET["firstName"];
+$lname = $_GET["lastName"];
+$trueFalse = $_GET["trueFalse"];
+$statements = $_GET["statements"];
+$majors = $_GET["majors"];
+$math1 = $_GET["mathQuestion1"];
+$math2 = $_GET["mathQuestion2"];
+
+
+#score tracker -368
+$score = 0;
+
+echo("<h1> $fname $lname's Results: </h1>");
+
+echo("Q1: Australia is wider than the moon:");
+
+echo("<br>");
+
+
+if ($trueFalse == "True") {
+    echo("CORRECT. Austrialia is wider than the Moon!");
+    $score++;
+}
+else{
+    echo("FALSE. Austrialia is wider than the Moon!");
+}
+
+echo("<br><br>");
+
+echo("Q2: Select all true statements");
+
+echo("<br>");
+
+if ($statements == "madonna") {
+    echo("CORRECT. Madonna's real name is Madonna");
+    $score++;
+}
+else {
+    echo("FALSE. K is worth four points in Scrabble! AND The only letter not in the periodic table is the letter J");
+}
+
+echo("<br>");
+echo("<br>");
+
+echo("Q3: What major is not offered in the SCI School");
+
+echo("<br>");
+
+if ($statements == "madonna") {
+    echo("CORRECT. Biology is not offered!");
+    $score++;
+}
+else {
+    echo("FALSE. Biology is not offered");
+}
+
+echo("<br>");
+echo("<br>");
+
+echo("Q4: 2^6 is equal to...");
+
+echo("<br>");
+
+if ($statements == "64") {
+    echo("CORRECT.");
+    $score++;
+}
+else {
+    echo("FALSE. Correct answer is 64");
+}
+
+echo("<br>");
+echo("<br>");
+
+echo("Q5: Log(1) is equal to...");
+
+if ($statements == "0") {
+    echo("CORRECT.");
+    $score++;
+}
+else {
+    echo("FALSE. Correct answer is 0");
+}
+
+echo("<br>");
+echo("<br>");
+
+echo("<h2>Total score : $score / 5 </h2>");
+
+$blob = array("FirstName"=>$fname, "LastName"=>$lname, "Q1"=>$trueFalse, "Q2"=>$statements, "Q3"=>$majors, "Q4"=>$math1, "Q5"=>$math2);
+$filePath = "results.txt";
+file_put_contents($filePath, $blob+";");
+
+
+?>
+
+
+
