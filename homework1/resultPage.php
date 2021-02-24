24343e8b4efb115f6a0981c71ba2049f45d8d960
Ibrahims Macbook <ibjbaba@gmail.com>
2021-02-24 00:34:59 -0600

    displaying scores

--- homework1/resultPage.php
+++ homework1/resultPage.php
@@ -1,8 +1,20 @@
 <?php
     // at a later point, you can convert it back to array like:
+
+    $mappedKeys = array("FirstName", "LastName", "Q1", "Q2", "Q3", "Q4", "Q5", "Timestamp","Score");
+
+    function displayScore($v1, $v2) {
+        echo($v1.": ".$v2);
+      }
+
     $data = file_get_contents('results.txt');
     $sep_data = explode(";", $data);
-    echo '<pre>'; print_r($sep_data); echo '</pre>';
+    $sep_data = array_pop($sep_data)
+    foreach ($sep_data as $val){
+        $sep_input = explode(",", $val);
+        array_map(displayScore($v1, $v2), $mappedKeys, $sep_input;
+    }
+ 
 
 /*     $recoveredData = unserialize($data);
     echo '<pre>'; print_r($recoveredData); echo '</pre>';
